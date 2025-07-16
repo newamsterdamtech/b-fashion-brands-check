e82ff32740ce281ac076475cccdfa655cc3216c5
newamsterdamtech <maurice@newamsterdam.tech>
2025-07-16 13:31:07 +0200

    Update app-check-bas.py
    changed title

--- app-check-bas.py
+++ app-check-bas.py
@@ -214,7 +214,7 @@ def main():
     """,
     unsafe_allow_html=True)
     
-    st.title("Buurfashion Leverblok Updater (Stap 1: Lookup → Stap 2: Merge)")
+    st.title("B Fashion Brands Leverblok Updater (Stap 1: Lookup → Stap 2: Merge)")
 
     if "token" not in st.session_state:
         st.write("Stap 1: Inloggen")
